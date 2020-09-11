c7acf3a7d2fa696403f5db8bac5593104f882fc1
threater <threatern@gmail.com>
2020-09-11 23:27:40 +0200

    Hide Systemcontroller

--- LucyCalender/Controllers/HomeController.cs
+++ LucyCalender/Controllers/HomeController.cs
@@ -1,30 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using LucyCalender.Models;
 using System.IO;
 using Newtonsoft.Json;
-using System.Reflection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Hosting.Internal;
 
 namespace LucyCalender.Controllers
 {
     public class HomeController : Controller
     {
-        private IApplicationLifetime applicationLifetime;
         private readonly ILogger<HomeController> _logger;
         private IHostingEnvironment _hostingEnvironment;
 
-        public HomeController(ILogger<HomeController> logger, IHostingEnvironment environment, IApplicationLifetime appLifetime)
+        public HomeController(ILogger<HomeController> logger, IHostingEnvironment environment)
         {
             _logger = logger;
             _hostingEnvironment = environment;
-            applicationLifetime = appLifetime;
         }
 
         public IActionResult Index()
